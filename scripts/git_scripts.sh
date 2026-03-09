6a05416eadb030216dd78208293cb4d0e5dd7e94
fang <fang@anthropic.com>
2026-03-09 11:11:40 -0700

    Add more git_scripts

--- scripts/git_scripts.sh
+++ scripts/git_scripts.sh
@@ -38,9 +38,9 @@ function goto {
 alias gbr='g checkout $(g branch --sort=-committerdate| sed "s/^[ *]*//" | fzf --height 40% --layout=reverse)'
 
 function gbD {
-  # checkout to Main and delete the current branch
+  # checkout to master/main/staging and delete the current branch
   CURR_BRANCH=$(git branch | grep "*" | cut -d" " -f2)
-  git checkout main
+  git checkout staging
   git branch -D $CURR_BRANCH
   git pull
 }
@@ -56,3 +56,44 @@ function gbDD {
       git branch -D "${branch}"
   done
 }
+
+function guri {
+    # Converts a string like "3f9ee86da22d sandbox/sandbox/w/render.py:108-169"
+    # to a GitHub URL like .../blob/3f9ee86da22d/sandbox/sandbox//w/render.py#L108-L169
+
+    if [ $# -eq 0 ]; then
+        echo "Usage: guri '<commit> <file_path>:<line_range>'" >&2
+        echo "   or: guri <commit> <file_path>:<line_range>" >&2
+        exit 1
+    fi
+
+    # Handle both "commit file:lines" (1 arg) and commit file:lines (2 args)
+    if [ $# -eq 1 ]; then
+        commit=$(echo "$1" | awk '{print $1}')
+        file_with_lines=$(echo "$1" | awk '{print $2}')
+    else
+        commit="$1"
+        file_with_lines="$2"
+    fi
+
+    # Split file path and line range
+    file_path=$(echo "$file_with_lines" | cut -d: -f1)
+    line_range=$(echo "$file_with_lines" | cut -d: -f2)
+
+    # Convert line range (108-169 -> L108-L169)
+    if [[ "$line_range" == *-* ]]; then
+        start_line=$(echo "$line_range" | cut -d- -f1)
+        end_line=$(echo "$line_range" | cut -d- -f2)
+        line_fragment="#L${start_line}-L${end_line}"
+    elif [[ "$line_range" =~ ^[0-9]+$ ]]; then
+        line_fragment="#L${line_range}"
+    else
+        line_fragment=""
+    fi
+
+    # Build the URL / TODO
+    url="https://github.com//blob/${commit}/${file_path}${line_fragment}"
+
+    echo "$url" | tee >(pbcopy)
+}
+
